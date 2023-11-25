82fe58dc6020b5e0a98661ff2c4a2ba664525655
Mario-Daoud <mario.daoud@student.vives.be>
2023-11-25 01:35:52 +0100

    initial commit

---
+++ src/main/java/com/example/wintersport/WintersportApplication.java
@@ -0,0 +1,13 @@
+package com.example.wintersport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintersportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WintersportApplication.class, args);
+	}
+
+}
