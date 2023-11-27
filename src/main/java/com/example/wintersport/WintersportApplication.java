f77db525379f5ba54f1ccba2fbefa1c11173d3d0
mario <r0895582@student.vives.be>
2023-11-27 21:13:09 +0100

    add blank domains

--- src/main/java/com/example/wintersport/WintersportApplication.java
+++ src/main/java/com/example/wintersport/WintersportApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WintersportApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WintersportApplication.class, args);
 	}
-
 }
