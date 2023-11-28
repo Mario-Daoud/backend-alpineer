dcae6ca9e1a8ad41fd5308eb14fd893d52592db3
mario <r0895582@student.vives.be>
2023-11-28 11:09:48 +0100

    add country model

--- src/main/java/com/example/wintersport/domain/Country.java
+++ src/main/java/com/example/wintersport/domain/Country.java
@@ -1,4 +1,56 @@
 package com.example.wintersport.domain;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "countries")
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "country", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Location> locations;
+
+    public Country() {}
+
+    public Country(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.locations = new HashSet<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(Set<Location> locations) {
+        this.locations = locations;
+    }
+
+    public void addLocation(Location location) {
+        locations.add(location);
+        location.setCountry(this);
+    }
 }
