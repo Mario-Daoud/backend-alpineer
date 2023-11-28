7084351ede3a535a1d7ad2d2b8e83ecd950ff4c3
mario <r0895582@student.vives.be>
2023-11-28 11:10:01 +0100

    add country repository

--- src/main/java/com/example/wintersport/repository/CountryRepository.java
+++ src/main/java/com/example/wintersport/repository/CountryRepository.java
@@ -1,2 +1,12 @@
-package com.example.wintersport.repository;public class CountryRepository {
+package com.example.wintersport.repository;
+
+import com.example.wintersport.domain.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+    Country findByName(String name);
+    List<Country> findAll();
+
 }
