d3420bbb3e398ffc1b047a99ecde4a91e58a15df
mario <r0895582@student.vives.be>
2023-11-28 11:22:07 +0100

    add optionals to repository

--- src/main/java/com/example/wintersport/repository/CountryRepository.java
+++ src/main/java/com/example/wintersport/repository/CountryRepository.java
@@ -4,9 +4,10 @@ import com.example.wintersport.domain.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
-    Country findByName(String name);
+    Optional<Country> findByName(String countryName);
     List<Country> findAll();
 
 }
--- src/main/java/com/example/wintersport/repository/LocationRepository.java
+++ src/main/java/com/example/wintersport/repository/LocationRepository.java
@@ -1,2 +1,13 @@
-package com.example.wintersport.repository;public interface LocationRepository {
+package com.example.wintersport.repository;
+
+import com.example.wintersport.domain.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    List<Location> findAll();
+    Optional<Location> findByName(String locationName);
+    Optional<List<Location>> findAllByCountry(String countryName);
 }
