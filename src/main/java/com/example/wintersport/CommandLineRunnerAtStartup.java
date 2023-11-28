51d975f93d1cd9542ae8694fb313a51a653c5909
mario <r0895582@student.vives.be>
2023-11-28 22:26:03 +0100

    add user model and repository

--- src/main/java/com/example/wintersport/CommandLineRunnerAtStartup.java
+++ src/main/java/com/example/wintersport/CommandLineRunnerAtStartup.java
@@ -3,9 +3,11 @@ package com.example.wintersport;
 import com.example.wintersport.domain.Country;
 import com.example.wintersport.domain.Location;
 import com.example.wintersport.domain.Sport;
+import com.example.wintersport.domain.User;
 import com.example.wintersport.repository.CountryRepository;
 import com.example.wintersport.repository.LocationRepository;
 import com.example.wintersport.repository.SportRepository;
+import com.example.wintersport.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,10 +20,12 @@ public class CommandLineRunnerAtStartup implements CommandLineRunner {
     private CountryRepository countryRepository;
     private LocationRepository locationRepository;
     private SportRepository sportRepository;
-    public CommandLineRunnerAtStartup(CountryRepository countryRepository, LocationRepository locationRepository, SportRepository sportRepository) {
+    private UserRepository userRepository;
+    public CommandLineRunnerAtStartup(CountryRepository countryRepository, LocationRepository locationRepository, SportRepository sportRepository, UserRepository userRepository) {
         this.countryRepository = countryRepository;
         this.locationRepository = locationRepository;
         this.sportRepository = sportRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -52,6 +56,14 @@ public class CommandLineRunnerAtStartup implements CommandLineRunner {
             s1.setLocations(locations);
             l1.setSports(sports);
             sportRepository.save(s1);
+        }
+
+        User u1 = new User();
+        u1.setUsername("username");
+
+        u1.setPassword("password");
+        if (userRepository.findByUsername(u1.getUsername()).isEmpty()) {
+            userRepository.save(u1);
         }
 
     }
--- src/main/java/com/example/wintersport/domain/User.java
+++ src/main/java/com/example/wintersport/domain/User.java
@@ -1,4 +1,49 @@
 package com.example.wintersport.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+
+    public User() {
+    }
+
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
