cb235d225a7e9a9bda9f36500dfb13a762f5101c
mario <r0895582@student.vives.be>
2023-12-10 11:41:14 +0100

    add repository annotation

--- src/main/java/com/example/wintersport/repository/LocationRepository.java
+++ src/main/java/com/example/wintersport/repository/LocationRepository.java
@@ -4,10 +4,12 @@ import com.example.wintersport.domain.Location;
 import com.example.wintersport.domain.Review;
 import com.example.wintersport.domain.Sport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Optional<Location> findByName(String name);
     List<Location> findByCountryName(String countryName);
--- src/main/java/com/example/wintersport/repository/UserRepository.java
+++ src/main/java/com/example/wintersport/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.wintersport.repository;
 
 import com.example.wintersport.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
     Optional<User> findByUsername(String username);
