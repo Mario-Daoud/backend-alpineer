9b2f6ebb87d001bf82a9653a2c3ab610e5c5a998
mario <r0895582@student.vives.be>
2023-12-13 02:23:01 +0100

    add all tests

--- src/test/java/com/example/wintersport/controller/ReviewControllerTest.java
+++ src/test/java/com/example/wintersport/controller/ReviewControllerTest.java
@@ -11,6 +11,7 @@ import com.example.wintersport.repository.UserRepository;
 import com.example.wintersport.request.ReviewRequest;
 import com.example.wintersport.response.ReviewResponse;
 import com.example.wintersport.service.ReviewService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -27,7 +28,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.mockito.Mockito.when;
@@ -62,55 +62,26 @@ public class ReviewControllerTest {
         reviews = new ArrayList<>();
         users = new ArrayList<>();
         locations = new ArrayList<>();
+        createTestData();
     }
 
     @Test
     void getAllReviewsExisting() throws Exception {
-        Country country = new Country();
-        country.setId(1L);
-        country.setName("test");
-
-        Location location = new Location();
-        location.setName("test");
-        location.setChairlifts(1);
-        location.setDegrees(1);
-        location.setDescription("test");
-        location.setSnowHeight(100);
-        location.setTrackLength(100);
-        location.setId(1L);
-
-        location.setCountry(country);
-        country.setLocations(Set.of(location));
-
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("test");
-        user.setId(1L);
-
-        Review review = new Review();
-        review.setId(1L);
-        review.setRating(5);
-        review.setUser(user);
-        review.setLocation(location);
-        reviews.add(review);
-
         when(reviewRepository.findAll()).thenReturn(reviews);
 
         mockMvc.perform(get(baseUrl))
-                .andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].rating").value(review.getRating()))
-                .andExpect(jsonPath("$[0].user").value(user.getUsername()))
-                .andExpect(jsonPath("$[0].location").value(location.getName()));
+                .andExpect(jsonPath("$[0].rating").value(reviews.getFirst().getRating()))
+                .andExpect(jsonPath("$[0].user").value(users.getFirst().getUsername()))
+                .andExpect(jsonPath("$[0].location").value(locations.getFirst().getName()));
     }
 
     @Test
     void getAllReviewsNonExisting() throws Exception {
-        when(reviewRepository.findAll()).thenReturn(reviews);
+        when(reviewRepository.findAll()).thenReturn(Collections.emptyList());
 
         mockMvc.perform(get(baseUrl))
-                .andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isEmpty());
@@ -118,288 +89,146 @@ public class ReviewControllerTest {
 
     @Test
     void getReviewsByLocationIdExisting() throws Exception {
-        Country country = new Country();
-        country.setId(1L);
-        country.setName("test");
-
-        Location location = new Location();
-        location.setName("test");
-        location.setChairlifts(1);
-        location.setDegrees(1);
-        location.setDescription("test");
-        location.setSnowHeight(100);
-        location.setTrackLength(100);
-        location.setId(1L);
-
-        location.setCountry(country);
-        country.setLocations(Set.of(location));
-
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("test");
-        user.setId(1L);
-
-        Review review = new Review();
-        review.setId(1L);
-        review.setRating(5);
-        review.setUser(user);
-        review.setLocation(location);
-        reviews.add(review);
-        location.setReviews(Set.of(review));
-
-        when(reviewRepository.findByLocationId(1L)).thenReturn(Optional.of(reviews));
+        when(reviewRepository.findByLocationId(reviews.getFirst().getId())).thenReturn(Optional.of(Collections.singletonList(reviews.getFirst())));
 
         mockMvc.perform(get(baseUrl + "/location/1"))
-                .andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].rating").value(review.getRating()))
-                .andExpect(jsonPath("$[0].username").value(user.getUsername()));
+                .andExpect(jsonPath("$[0].rating").value(reviews.getFirst().getRating()))
+                .andExpect(jsonPath("$[0].username").value(users.getFirst().getUsername()));
     }
 
     @Test
     void getReviewsByLocationIdNonExisting() throws Exception {
-        when(reviewRepository.findByLocationId(1L)).thenReturn(Optional.empty());
+        when(reviewRepository.findByLocationId(reviews.getFirst().getId())).thenReturn(Optional.empty());
 
         mockMvc.perform(get(baseUrl + "/location/1"))
-                .andDo(print())
                 .andExpect(status().isNotFound());
     }
 
     @Test
     void getReviewsByUserIdExisting() throws Exception {
-        Country country = new Country();
-        country.setId(1L);
-        country.setName("test");
+        when(reviewRepository.findByUserId(reviews.getFirst().getId())).thenReturn(Optional.of(Collections.singletonList(reviews.getFirst())));
 
-        Location location = new Location();
-        location.setName("test");
-        location.setChairlifts(1);
-        location.setDegrees(1);
-        location.setDescription("test");
-        location.setSnowHeight(100);
-        location.setTrackLength(100);
-        location.setId(1L);
-
-        location.setCountry(country);
-        country.setLocations(Set.of(location));
-
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("test");
-        user.setId(1L);
-
-        Review review = new Review();
-        review.setId(1L);
-        review.setRating(5);
-        review.setUser(user);
-        review.setLocation(location);
-        reviews.add(review);
-
-        when(reviewRepository.findByUserId(1L)).thenReturn(Optional.of(reviews));
-
-        mockMvc.perform(get(baseUrl + "/user/" + user.getId()))
-                .andDo(print())
+        mockMvc.perform(get(baseUrl + "/user/1"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].rating").value(review.getRating()))
-                .andExpect(jsonPath("$[0].location").value(location.getName()));
+                .andExpect(jsonPath("$[0].rating").value(reviews.getFirst().getRating()))
+                .andExpect(jsonPath("$[0].location").value(locations.getFirst().getName()));
     }
 
     @Test
     void getReviewsByUserIdNonExisting() throws Exception {
-        when(reviewRepository.findByUserId(1L)).thenReturn(Optional.empty());
+        when(reviewRepository.findByUserId(reviews.getFirst().getId())).thenReturn(Optional.empty());
 
         mockMvc.perform(get(baseUrl + "/user/1"))
-                .andDo(print())
                 .andExpect(status().isNotFound());
     }
 
     @Test
     void getReviewByIdExisting() throws Exception {
-        Country country = new Country();
-        country.setId(1L);
-        country.setName("test");
+        when(reviewRepository.findById(reviews.getFirst().getId())).thenReturn(Optional.of(reviews.getFirst()));
 
-        Location location = new Location();
-        location.setName("test");
-        location.setChairlifts(1);
-        location.setDegrees(1);
-        location.setDescription("test");
-        location.setSnowHeight(100);
-        location.setTrackLength(100);
-        location.setId(1L);
-
-        location.setCountry(country);
-        country.setLocations(Set.of(location));
-
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("test");
-        user.setId(1L);
-
-        Review review = new Review();
-        review.setId(1L);
-        review.setRating(5);
-        review.setUser(user);
-        review.setLocation(location);
-        reviews.add(review);
-
-        when(reviewRepository.findById(1L)).thenReturn(Optional.of(review));
-
-        mockMvc.perform(get(baseUrl + "/" + review.getId()))
-                .andDo(print())
+        mockMvc.perform(get(baseUrl + "/1"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.rating").value(review.getRating()))
-                .andExpect(jsonPath("$.user").value(user.getUsername()))
-                .andExpect(jsonPath("$.location").value(location.getName()));
+                .andExpect(jsonPath("$.rating").value(reviews.getFirst().getRating()))
+                .andExpect(jsonPath("$.user").value(users.getFirst().getUsername()))
+                .andExpect(jsonPath("$.location").value(locations.getFirst().getName()));
     }
 
     @Test
     void getReviewByIdNonExisting() throws Exception {
-        when(reviewRepository.findById(1L)).thenReturn(Optional.empty());
+        when(reviewRepository.findById(reviews.getFirst().getId())).thenReturn(Optional.empty());
 
         mockMvc.perform(get(baseUrl + "/1"))
-                .andDo(print())
                 .andExpect(status().isNotFound());
     }
 
-
     @Test
     void addReview() throws Exception {
-        Country country = new Country();
-        country.setId(1L);
-        country.setName("test");
+        when(countryRepository.save(any(Country.class))).thenReturn(countries.getFirst());
+        when(locationRepository.save(any(Location.class))).thenReturn(locations.getFirst());
+        when(reviewService.addReview(any(Long.class), any(Long.class), any(ReviewRequest.class)))
+                .thenReturn(new ReviewResponse(reviews.getFirst()));
+        when(reviewRepository.save(any(Review.class))).thenReturn(reviews.getFirst());
 
-        Location location = new Location();
-        location.setName("test");
-        location.setChairlifts(1);
-        location.setDegrees(1);
-        location.setDescription("test");
-        location.setSnowHeight(100);
-        location.setTrackLength(100);
-        location.setId(1L);
-
-        location.setCountry(country);
-        country.setLocations(Set.of(location));
-
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("test");
-        user.setId(1L);
-
-        Review review = new Review();
-        review.setId(1L);
-        review.setRating(5);
-        review.setUser(user);
-        review.setLocation(location);
-
-        when(countryRepository.save(any(Country.class))).thenReturn(country);
-        when(locationRepository.save(any(Location.class))).thenReturn(location);
-        when(reviewService.addReview(any(Long.class), any(Long.class), any(ReviewRequest.class))).thenReturn(new ReviewResponse(review));
-        when(reviewRepository.save(any(Review.class))).thenReturn(review);
-
-        ReviewRequest reviewRequest = new ReviewRequest();
-        reviewRequest.setRating(5);
-
-        mockMvc.perform(post("/api/review/{userId}/{locationId}", user.getId(), location.getId())
+        mockMvc.perform(post("/api/review/{userId}/{locationId}", users.getFirst().getId(), locations.getFirst().getId())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(reviewRequest)))
+                        .content(createReviewRequest()))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(header().string("location", "http://localhost/api/review/1/1/1"))
-                .andExpect(jsonPath("$.id").value(review.getId()))
-                .andExpect(jsonPath("$.rating").value(review.getRating()));
+                .andExpect(jsonPath("$.id").value(reviews.getFirst().getId()))
+                .andExpect(jsonPath("$.rating").value(reviews.getFirst().getRating()));
     }
 
     @Test
     void addReviewNonExistingUser() throws Exception {
-        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+        when(userRepository.findById(users.getFirst().getId())).thenReturn(Optional.empty());
 
-        ReviewRequest reviewRequest = new ReviewRequest();
-        reviewRequest.setRating(5);
-
-        mockMvc.perform(post("/api/review/{userId}/{locationId}", 1L, 1L)
+        mockMvc.perform(post("/api/review/{userId}/{locationId}", users.getFirst().getId(), locations.getFirst().getId())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(reviewRequest)))
+                        .content(createReviewRequest()))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     void addReviewNonExistingLocation() throws Exception {
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("test");
-        user.setId(1L);
+        when(userRepository.findById(users.getFirst().getId())).thenReturn(Optional.of(users.getFirst()));
+        when(locationRepository.findById(locations.getFirst().getId())).thenReturn(Optional.empty());
 
-        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-        when(locationRepository.findById(1L)).thenReturn(Optional.empty());
-
-        ReviewRequest reviewRequest = new ReviewRequest();
-        reviewRequest.setRating(5);
-
-        mockMvc.perform(post("/api/review/{userId}/{locationId}", 1L, 1L)
+        mockMvc.perform(post("/api/review/{userId}/{locationId}", users.getFirst().getId(), locations.getFirst().getId())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(reviewRequest)))
+                        .content(createReviewRequest()))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     void updateReviewExisting() throws Exception {
-        Country country = new Country();
-        country.setId(1L);
-        country.setName("test");
-
-        Location location = new Location();
-        location.setName("test");
-        location.setChairlifts(1);
-        location.setDegrees(1);
-        location.setDescription("test");
-        location.setSnowHeight(100);
-        location.setTrackLength(100);
-        location.setId(1L);
-
-        location.setCountry(country);
-        country.setLocations(Set.of(location));
-
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("test");
-        user.setId(1L);
-
-        Review review = new Review();
-        review.setId(1L);
-        review.setRating(3);
-        review.setUser(user);
-        review.setLocation(location);
-        reviews.add(review);
-
-        when(reviewRepository.findById(1L)).thenReturn(Optional.of(review));
+        when(reviewRepository.findById(reviews.getFirst().getId())).thenReturn(Optional.of(reviews.getFirst()));
 
         mockMvc.perform(put(baseUrl + "/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"rating\": 5}"))
-                .andDo(print())
+                        .content(createReviewRequest()))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(review.getId()))
-                .andExpect(jsonPath("$.rating").value(review.getRating()));
+                .andExpect(jsonPath("$.id").value(reviews.getFirst().getId()))
+                .andExpect(jsonPath("$.rating").value(reviews.getFirst().getRating()));
     }
 
     @Test
     void updateReviewNonExisting() throws Exception {
-        when(reviewRepository.findById(1L)).thenReturn(Optional.empty());
+        when(reviewRepository.findById(reviews.getFirst().getId())).thenReturn(Optional.empty());
 
         mockMvc.perform(put(baseUrl + "/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"rating\": 5}"))
-                .andDo(print())
+                        .content(createReviewRequest()))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     void deleteReviewExisting() throws Exception {
+        when(userRepository.findById(users.getFirst().getId())).thenReturn(Optional.of(users.getFirst()));
+        when(locationRepository.findById(locations.getFirst().getId())).thenReturn(Optional.of(locations.getFirst()));
+        when(reviewRepository.findById(reviews.getFirst().getId())).thenReturn(Optional.of(reviews.getFirst()));
+
+        mockMvc.perform(delete(baseUrl + "/1"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(reviews.getFirst().getId()))
+                .andExpect(jsonPath("$.rating").value(reviews.getFirst().getRating()));
+    }
+
+    @Test
+    void deleteReviewNonExisting() throws Exception {
+        when(reviewRepository.findById(reviews.getFirst().getId())).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete(baseUrl + "/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    private void createTestData() {
         Country country = new Country();
         country.setId(1L);
         country.setName("test");
@@ -412,9 +241,7 @@ public class ReviewControllerTest {
         location.setSnowHeight(100);
         location.setTrackLength(100);
         location.setId(1L);
-
         location.setCountry(country);
-        country.setLocations(Set.of(location));
 
         User user = new User();
         user.setUsername("test");
@@ -426,27 +253,17 @@ public class ReviewControllerTest {
         review.setRating(5);
         review.setUser(user);
         review.setLocation(location);
+
+        countries.add(country);
+        locations.add(location);
+        users.add(user);
         reviews.add(review);
-
-        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-        when(locationRepository.findById(1L)).thenReturn(Optional.of(location));
-
-        when(reviewRepository.findById(1L)).thenReturn(Optional.of(review));
-
-        mockMvc.perform(delete(baseUrl + "/1"))
-                .andDo(print())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(review.getId()))
-                .andExpect(jsonPath("$.rating").value(review.getRating()));
     }
 
-    @Test
-    void deleteReviewNonExisting() throws Exception {
-        when(reviewRepository.findById(1L)).thenReturn(Optional.empty());
-
-        mockMvc.perform(delete(baseUrl + "/1"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
+    String createReviewRequest() throws JsonProcessingException {
+        ReviewRequest reviewRequest = new ReviewRequest();
+        reviewRequest.setRating(5);
+        return objectMapper.writeValueAsString(reviewRequest);
     }
 }
+
